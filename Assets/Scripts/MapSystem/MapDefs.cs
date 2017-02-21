89fe83aeba7241c43abef416543271fabe02e917
Molnár Gergely <bigyo200@gmail.com>
2017-02-21 20:08:52 +0100

    Bzz

---
+++ Assets/Scripts/MapSystem/MapDefs.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDefs {
+	
+	public class LabirinthMap
+	{
+		public int height = 10;
+		public int width = 10;
+
+		public bool loaded = false;
+		public bool[] checkboard;
+
+		public void ResetMap(int nHeight = int.MinValue, int nWidth = int.MinValue)
+		{
+			if (nHeight != int.MinValue)
+				height = nHeight;
+
+			if (nWidth != int.MinValue)
+				width = nWidth;
+
+			checkboard = new bool[height * width];
+
+			for (int h = 0; h < height; h++)
+				for (int w = 0; w < width; w++)
+					checkboard[h*w] = false;
+
+			loaded = false;
+		}
+
+		//public void 
+	}
+
+	public class TreeMapNode
+	{
+		public TreeMapNode parent;
+		public List<TreeMapNode> children;
+
+		public bool wall = false;
+
+	}
+
+}
